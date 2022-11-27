bb334ede51b52919faf6d53fe19c22d373241a98
markus b <mnogo_bukof@mail.ru>
2022-11-27 23:17:02 +0700

    первое задание

---
+++ Первое задание/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.*/
+System.Console.WriteLine("Введите значение числа N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+void PrintNumber(int n, int count)
+{
+  if (count > n) return;
+  PrintNumber(n, count + 1);
+  Console.Write($"Натуральное число:[{count}], ");
+}
+
+PrintNumber(n, count);
+System.Console.WriteLine();
+
