da16c1c6d9b5e33b121992128912817fa2266a96
markus b <mnogo_bukof@mail.ru>
2022-11-27 23:26:20 +0700

    третье задание

--- Третье задание/Program.cs
+++ Третье задание/Program.cs
@@ -4,13 +4,13 @@ System.Console.WriteLine("Введите число M: ");
 int M = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int functionAkkererman = Akker(M, N);
+int FunctionAkkerman = Akkerman(M, N);
 
-int Akker(int M, int N)
+int Akkerman(int M, int N)
 {
   if (M == 0) return N + 1;
-  else if (N == 0) return Akker(M - 1, 1);
-  else return Akker(M - 1, Akker(M, N - 1));
+  else if (N == 0) return Akkerman(M - 1, 1);
+  else return Akkerman(M - 1, Akkerman(M, N - 1));
 }
-System.Console.WriteLine($"Функция Аккермана ({M};{N}) = {functionAkkererman}. ");
+System.Console.WriteLine($"Функция Аккермана ({M};{N}) = ({FunctionAkkerman}). ");
 
